6a413cecb9f3e22a7f94543726d7fa51ad50a9a4
rubycabot <58884396+rubycabot@users.noreply.github.com>
2023-03-28 16:56:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "28809795"))
+    API_HASH = os.environ.get("API_HASH", "e45a8c555ac1a56d1aa1a80db599489a")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6230948304:AAGU4YLNxKGk5D7DC2GjH4lsoNqr6DB-hf0")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
-    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
+    OWNER_ID = os.environ.get("OWNER_ID", "5140363414")                             
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ruby9ca:ruby@N123@ruby-cluster0.jujt8fe.mongodb.net/?retryWrites=true&w=majority")  
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "ruby-Cluster0")
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'my')
     SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001986323674"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
 
 
